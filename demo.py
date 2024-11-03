f46a3ee9304623a6bec3f8cc25785ec93b83409b
Russel Arbore <rarbore2@illinois.edu>
2024-11-03 15:02:13 -0600

    draft demo

--- demo.py
+++ demo.py
@@ -5,7 +5,33 @@ print("=========================================")
 print("            HD-Classification            ")
 print("=========================================")
 
-#TODO: Fill with HD-Classification commands once that's ready.
+os.chdir("HD-Classification/Hetero-C++")
+myenv = os.environ.copy()
+
+while True:
+	lower_bound = input("Enter a target test accuracy. ")
+	result = subprocess.run(["python3", "lookup_cfg.py", str(lower_bound)], capture_output=True)
+	cfg = str(result.stdout).split(" ")
+	if "None" in cfg[0]:
+		print("Please input a viable test accuracy.")
+	else:
+		mlc = int(cfg[0].split("'")[1])
+		wv = int(cfg[1].split("\\")[0])
+		print("From look-up table, using", mlc, "MLC bits and", wv, "write-verify cycles will achieve >=", lower_bound, "test accuracy.")
+		break
+
+print("")
+print("Compiling HD-Classification.")
+print("")
+myenv["MLC"] = str(mlc)
+myenv["WRITE_VERIFY"] = str(wv)
+subprocess.run(["make", "clean"], env=myenv)
+subprocess.run(["make", "-j", "host-sim"], env=myenv)
+subprocess.run(["./host-sim", "1"])
+print("")
+input("Press enter to move on to HD-Clustering.")
+
+os.chdir("../..")
 
 subprocess.run("clear")
 print("=========================================")
@@ -24,7 +50,7 @@ while True:
 	else:
 		mlc = int(cfg[0].split("'")[1])
 		wv = int(cfg[1].split("\\")[0])
-		print("From look-up table, using", mlc, "MLC bits and", wv, "write-verify cycles will achieve", lower_bound, "mutual info score.")
+		print("From look-up table, using", mlc, "MLC bits and", wv, "write-verify cycles will achieve >=", lower_bound, "mutual info score.")
 		break
 
 print("")
@@ -32,8 +58,11 @@ print("Compiling HD-Clustering.")
 print("")
 myenv["MLC"] = str(mlc)
 myenv["WRITE_VERIFY"] = str(wv)
-subprocess.run(["make", "clean", "host-sim"], env=myenv)
+subprocess.run(["make", "clean"], env=myenv)
+subprocess.run(["make", "-j", "host-sim"], env=myenv)
 subprocess.run(["./host-sim", "3"])
 subprocess.run(["python3", "mutual_info.py"])
+print("")
+input("Press enter to exit.")
 
 os.chdir("../..")
